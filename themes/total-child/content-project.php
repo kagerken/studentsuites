5bf5ff17fe2ed59875afe1c3ba8fe0bbb2252638
Unknown <kagerken@live.com>
2018-03-19 10:37:12 -0500

    Adding Project Archive Template

---
+++ themes/total-child/content-project.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+
+ * Template part for displaying Projects with Thumbnail Image / Heading.
+
+ *
+
+ * @package Total
+
+ */
+
+
+
+?>
+
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('total-hentry'); ?>>
+
+	<?php if ( 'post' == get_post_type() ) : ?>
+
+	<div class="entry-meta ht-post-info">
+
+		<?php total_posted_on(); ?>
+
+	</div><!-- .entry-meta -->
+
+	<?php endif; ?>
+
+
+
+
+
+	<div class="ht-post-wrapper">
+
+		<?php if(has_post_thumbnail()): ?>
+
+		<figure class="entry-figure">
+
+			<?php 
+
+			$total_image = wp_get_attachment_image_src( get_post_thumbnail_id() , 'total-portfolio-thumb' );
+
+			?>
+
+			<a href="<?php the_permalink(); ?>"><img src="<?php echo esc_url($total_image[0]); ?>" alt="<?php echo esc_attr( get_the_title() ) ?>"></a>
+
+		</figure>
+
+		<?php endif; ?>
+
+        
+
+		<header class="entry-header">
+
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+		</header><!-- .entry-header -->
+
+
+	</div>
+
+</article><!-- #post-## -->
+
