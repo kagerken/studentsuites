9308a11523256ccd55b80e1cc14a256ec01d6c70
Unknown <kagerken@live.com>
2018-03-30 18:03:02 -0500

    Redefine Home Page Template

---
+++ themes/total-child/home-template.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+
+ * Template Name: Student Suites Home Page
+
+ *
+
+ * @package total-child
+
+ */
+
+
+
+get_header();
+
+
+
+		get_template_part('template-parts/section', 'slider');
+
+?>
+
+
+
+    <div>
+
+        <p>Words go here</p>
+
+    </div>
+
+
+
+
+
+<?php get_footer(); ?>
