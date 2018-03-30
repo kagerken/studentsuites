eae9e148d6cc4c5b73dab396212facc8df3aca7d
Unknown <kagerken@live.com>
2018-03-29 23:56:54 -0500

    Template Styles

--- themes/total-child/template-parts/content-single.php
+++ themes/total-child/template-parts/content-single.php
@@ -8,24 +8,20 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-content">
-		<div class="entry-meta ht-post-info">
-			<?php total_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		
-		<div>
-			<div class="project-information-container">
-				<div><?php the_field('date_completed');?></div>
-				<div><a href="<?php the_field('file_attachements') ?>" target="_blank">Download Project Case Study</a></div>
-				<div>$<?php the_field('project_cost');?></div>
-				<div># of beds: <?php the_field('project_beds');?></div>
-				<div>$<?php the_field('per_bed_cost');?> per bed</div>
-				<div>$<?php the_field('square_footage_cost');?> per square foot</div>
-			</div>
-			<div class="project-photo-container">
+		<div class="grid-row m-padding-vertical">
+			<div class="project-photo-container s-padding-horizontal">
 				<?php the_post_thumbnail('total-blog-header'); // I need to define a custom one in our theme functions
 				?>
 			</div>
+			<div class="project-information-container s-padding-horizontal">
+				<div><span class="xs-r-margin bolded">Date Completed:</span><?php the_field('date_completed');?></div>
+				<div><span class="xs-r-margin bolded">Total Cost:</span>$<?php the_field('project_cost');?></div>
+				<div><span class="xs-r-margin bolded"># of Beds:</span><?php the_field('project_beds');?></div>
+				<div><span class="xs-r-margin bolded">Per Bed Cost:</span>$<?php the_field('per_bed_cost');?></div>
+				<div><span class="xs-r-margin bolded">Per Square Foot:</span>$<?php the_field('square_footage_cost');?></div>
+				<div><a href="<?php the_field('file_attachements') ?>" target="_blank">Download Project Case Study</a></div>
+			</div>
+			
 		</div>
 		<div class="content"><?php the_content(); ?></div>
 
