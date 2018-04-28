afc0be3c046fd6fe0e87b78e94fe75a19f05ec07
talia-storymaker <storymakerechidna@gmail.com>
2018-04-28 15:12:32 -0500

    New attempt to fix
    Decided to stop trying to be clever adding the script to the functions.php file and instead just adding the script directly to the template.

--- themes/total-child/archive-projects.php
+++ themes/total-child/archive-projects.php
@@ -50,4 +50,18 @@ get_header(); ?>
 
 </div>
 
+<script>
+	function heightEqualsWidth() {
+		console.log('test');
+		var figures = document.getElementsByClassName('entry-figure');
+		var entriesWidth = figures[0].offsetWidth;
+		for (var i = figures.length; i > 0; i++) {
+			figures[i].style.height = entriesWidth;
+		}
+	}
+
+	heightEqualsWidth();
+	window.addEventListener('resize', heightEqualsWidth);
+</script>
+
 <?php get_footer(); ?>
--- themes/total-child/functions.php
+++ themes/total-child/functions.php
@@ -3,8 +3,5 @@
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-function test_enqueue_scripts() {
-    wp_enqueue_script( 'All Projects scripts', get_template_directory_uri() . '/js/allprojectsscripts.js', array(), true );
-}
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 ?>
