aa073df6516f8bbb92a298bd6fb4972a24a956b8
talia-storymaker <storymakerechidna@gmail.com>
2018-04-28 15:04:40 -0500

    Rebreaking the site
    I purposefully reinstated the site-breaking code so I can read the error message again and hopefully untangle the problem more successfully.

--- themes/total-child/functions.php
+++ themes/total-child/functions.php
@@ -3,8 +3,8 @@
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-/*function wp_enqueue_scripts() {
+function wp_enqueue_scripts() {
     wp_enqueue_script( 'All Projects scripts', get_template_directory_uri() . '/js/allprojectsscripts.js', array(), true );
-}*/
+}
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 ?>
