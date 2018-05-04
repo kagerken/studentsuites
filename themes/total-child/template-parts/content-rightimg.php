ececde2c4fc705548a92783a7cb719ec55eb071c
talia-storymaker <storymakerechidna@gmail.com>
2018-05-04 17:05:51 -0500

    Our Process - Making right images on the right again

--- themes/total-child/template-parts/content-rightimg.php
+++ themes/total-child/template-parts/content-rightimg.php
@@ -12,13 +12,12 @@
 <section class="right-image">
 	<h2><?php the_title() ?></h2>
 	<div class="grid-row grid-flex-break">
+		<div class="grid-item-1 m-a-padding">
+			<?php the_content(); ?>
+		</div>
 		<div class="grid-item-1 image-container m-h-padding">
 			<a href="<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full', false );
   echo $src[0]; ?>" target="_blank"><?php the_post_thumbnail(); ?></a>
 		</div>
-		<div class="grid-item-1 m-a-padding">
-			<?php the_content(); ?>
-		</div>
-		
 	</div>
 </section>
